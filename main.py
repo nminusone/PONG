e61503c34948977a9eab4b9fe2cf11184680c2d7
nminusone <obmar2055@yahoo.com>
2021-11-29 18:08:20 -0500

    Paddle class updates ball class development

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 import turtle as t
 from turtle import Turtle, Screen
-from paddle import Paddle
+from paddle import Paddle, Ball
 
 screen = Screen()
+# Tracer turns animation off
+screen.tracer(0)
 screen.setup(width=800, height=600)
 screen.bgcolor('black')
 screen.title('PONG')
-right_paddle = Paddle()
+
+right_paddle = Paddle(350, 0)
+left_paddle = Paddle(-350, 0)
+pong = Ball()
+
 screen.listen()
-screen.onkey(right_paddle.up, 'Up')
+screen.onkey(right_paddle.right_up, 'Up')
+screen.onkey(right_paddle.right_down, 'Down')
+screen.onkey(left_paddle.left_up, 'w')
+screen.onkey(left_paddle.left_down, 's')
+
+game_is_on = True
+while game_is_on:
+    # Updates when animation is off/Tracer(0)
+    screen.update()
+    pong.move_ball()
 
 screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -2,17 +2,51 @@ from turtle import Turtle
 
 WIDTH = 20
 HEIGHT = 100
-X_POS = 350
-Y_POS = 0
 
 
-class Paddle:
+# X_POS = 350
+# Y_POS = 0
+
+
+# x_cor, y_cor
+
+class Paddle(Turtle):
+    def __init__(self, x_cor, y_cor):
+        super().__init__()
+        self.shape('square')
+        self.x_init = x_cor
+        self.y_init = y_cor
+        self.color('white')
+        self.penup()
+        self.goto(self.x_init, self.y_init)
+
+        self.shapesize(5, 1, 1)
+        self.pad_pos = 0
+
+    def right_up(self):
+        self.pad_pos = self.ycor() + 20
+        self.goto(350, self.pad_pos)
+
+    def right_down(self):
+        self.pad_pos = self.ycor() - 20
+        self.goto(350, self.pad_pos)
+
+    def left_up(self):
+        self.pad_pos = self.ycor() + 20
+        self.goto(-350, self.pad_pos)
+
+    def left_down(self):
+        self.pad_pos = self.ycor() - 20
+        self.goto(-350, self.pad_pos)
+
+
+class Ball(Turtle):
     def __init__(self):
-        self.pad = Turtle(shape='square')
-        self.pad.color('white')
-        self.pad.penup()
-        self.pad.goto(X_POS, Y_POS)
-        self.pad.shapesize(5, 1, 1)
+        super().__init__()
+        self.shape('circle')
+        self.color('blue')
+        self.goto(0, 0)
 
-    def up(self):
-        self.pad.goto(X_POS, Y_POS + 5)
+    def move_ball(self):
+        self.seth(45)
+        self.forward(20)
